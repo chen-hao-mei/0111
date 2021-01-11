ee4a7608b327657662be7eb367d7430cbd67dd39
Administrator <18276039083****>
2021-01-11 15:15:03 +0800

    1.多态类。 2.方法重载。 3.飞机订票管理系统。

---
+++ src/planexitong/Customer.java
@@ -0,0 +1,40 @@
+package planexitong;
+
+public class Customer {
+    private String id;      //乘客ID
+    private String name;       //乘客姓名
+    private String phone;    //电话号码
+    private String cardid;  //身份证号
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getCardid() {
+        return cardid;
+    }
+
+    public void setCardid(String cardid) {
+        this.cardid = cardid;
+    }
+}
